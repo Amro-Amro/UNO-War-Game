21b35a39558ae52c638f777b9462de562deed069
Amro Abu-atieh <160271190+Amro-Amro@users.noreply.github.com>
2024-10-28 12:15:23 -0500

    Create AI.java

---
+++ AI.java
@@ -0,0 +1,22 @@
+public class AI {
+
+
+    public Card getPlay(Hand hand, CardPile cardPile){
+        if(hand == null){
+            return null;}
+
+        for(int i = 0; i < hand.getSize(); i++) {
+            Card card = hand.get(i);
+
+            if (cardPile.canPlay(card)) {
+                return card;
+            }
+        }
+            return null;
+
+    }
+
+    public String toString(){
+        return "Random Card AI";
+    }
+}
