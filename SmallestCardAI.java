aaf5c88de0b107d067ca294cb132f34f7517acde
Amro Abu-atieh <160271190+Amro-Amro@users.noreply.github.com>
2024-10-28 12:15:57 -0500

    Create SmallestCardAI.java

---
+++ SmallestCardAI.java
@@ -0,0 +1,31 @@
+public class SmallestCardAI {
+
+    @Override
+    public Card getPlay(Hand hand, CardPile cardPile){
+
+        if(hand == null || hand.getSize() == 0){
+            return null;}
+
+        Card min = null;
+
+        for(int i = 0; i < hand.getSize(); i++) {
+
+            Card card = hand.get(i);
+
+            if (cardPile.canPlay(card)) {
+
+                if(min == null || min.getRankNum() < card.getRankNum()){
+                    min = card;
+                }
+            }
+          }
+
+        return min;
+
+    }
+
+    @Override
+    public String toString(){
+        return "Smallest Card AI";
+    }
+}
