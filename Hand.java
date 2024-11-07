61b8093e084aa30b5182ccd79aff3ea385173bce
Amro Abu-atieh <160271190+Amro-Amro@users.noreply.github.com>
2024-11-07 11:40:37 -0600

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -7,8 +7,19 @@ package student;
  */
 public class Hand {
 
+    /**
+    * This makes an array of cards that will be used in the hand.
+    */
     private Card[] hand;
+
+    /**
+    * intitializing the integer i, that will be sued for indexing and for loops.
+    */
     private int i;
+
+    /**
+    * This makes a deck tht will be used in the hand class.
+    */
     private Deck deck;
 
     /**
@@ -39,14 +50,14 @@ public class Hand {
      * Retrieves the card at the specified index from the hand.
      * If the index is invalid, it returns the first card in the hand as a fallback.
      *
-     * @param i, index of card being retrieved
+     * @param i index of card being retrieved
      * @return the card at the specified index, or the first card if the index is invalid
      */
     public Card get(int i) {
         if (i >= hand.length || i < 0) {
             System.out.println("Invalid hand index!");
             return hand[0];
-        } else {return hand[i];}
+        } else { return hand[i]; }
     }
 
     /**
