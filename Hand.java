e2d9ad1d1962def400b1dac5c19a5133109f03c2
Amro Abu-atieh <160271190+Amro-Amro@users.noreply.github.com>
2024-11-07 10:54:51 -0600

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,31 +1,61 @@
 package student;
 
+/**
+ * The Hand class represents a player's hand in a card game.
+ * It stores a set of Card objects and allows for operations
+ * like drawing from a deck, removing cards, and refilling the hand.
+ */
 public class Hand {
 
     private Card[] hand;
     private int i;
     private Deck deck;
 
-    public Hand(Deck deck, int size){
+    /**
+     * This constructor makes a hand by drawing a specified number of cards
+     * from the given deck.
+     *
+     * @param deck the deck to draw cards from
+     * @param size the number of cards to initialize in the hand
+     */
+    public Hand(Deck deck, int size) {
         this.deck = deck;
         hand = new Card[size];
 
-        for(i = 0; i < size; i++){
+        for (i = 0; i < size; i++) {
             hand[i] = deck.draw();
         }
     }
 
-    public int getSize(){
+    /**
+     *
+     * @return the number of cards in the hand (size)
+     */
+    public int getSize() {
         return hand.length;
     }
 
-    public Card get(int i){
-        if(i >= hand.length || i < 0){
+    /**
+     * Retrieves the card at the specified index from the hand.
+     * If the index is invalid, it returns the first card in the hand as a fallback.
+     *
+     * @param i, index of card being retrieved
+     * @return the card at the specified index, or the first card if the index is invalid
+     */
+    public Card get(int i) {
+        if (i >= hand.length || i < 0) {
             System.out.println("Invalid hand index!");
             return hand[0];
         } else {return hand[i];}
     }
 
+    /**
+     * Removes the specified card from the hand and replaces it with a new card drawn from the deck.
+     *
+     * @param card the card to be removed
+     * @return true if the card was successfully removed, and
+     *         false if the card was not found in the hand
+     */
     public boolean remove(Card card) {
         if (card == null) {
             return false;
@@ -33,13 +63,17 @@ public class Hand {
 
         for (int j = 0; j < hand.length; j++) {
             if (hand[j].equals(card)) {
-                hand[j] = deck.draw(); 
+                hand[j] = deck.draw(); // Or shift cards to keep hand intact
                 return true;
             }
         }
         return false; // Card not found
     }
 
+    /**
+     * Refills the hand by drawing new cards from the deck to replace the null spots.
+     * This ensures that the hand is fully refilled, and ready.
+     */
     public void refill() {
         for (int i = 0; i < hand.length; i++) {
             if (hand[i] == null) {
